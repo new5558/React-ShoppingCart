1b49c7ff258417b9bdb701c4a94a00502aae069d
new5558 <new17353@gmail.com>
2019-01-08 01:08:59 +0700

    add build file

---
+++ build/precache-manifest.20f03178a208aa1728ac659e08c4960c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "108f57abbf51a1fd3231",
+    "url": "/static/js/main.108f57ab.chunk.js"
+  },
+  {
+    "revision": "80257abb1b98bb5a10c6",
+    "url": "/static/js/1.80257abb.chunk.js"
+  },
+  {
+    "revision": "108f57abbf51a1fd3231",
+    "url": "/static/css/main.65e3b47e.chunk.css"
+  },
+  {
+    "revision": "80257abb1b98bb5a10c6",
+    "url": "/static/css/1.df911f12.chunk.css"
+  },
+  {
+    "revision": "9a23a22fac7a007c0c28dd34d8797f16",
+    "url": "/index.html"
+  }
+];
